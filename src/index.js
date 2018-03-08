81bb32349995f06a0994c8d50e90c823d14d7f8a
Horacio Gonzalez <horacio.gonzalez@corp.ovh.com>
2018-03-08 12:01:28 +0100

    Updating to custom elements v1

--- src/index.js
+++ src/index.js
@@ -2,34 +2,54 @@ import { h, render } from 'preact';
 
 const Empty = () => null;
 
-export default function register(Component, tagName) {
-	let prototype = Object.create(HTMLElement.prototype);
-	prototype._vdomComponent = Component;
-	prototype.attachedCallback = prototype.attributeChangedCallback = renderElement;
-	prototype.detachedCallback = unRenderElement;
-	return document.registerElement(
-		tagName || Component.displayName || Component.name,
-		{ prototype }
-	);
-}
-
-function renderElement() {
-	this._root = render(
-		toVdom(this, this._vdomComponent),
-		this.shadowRoot || this.createShadowRoot(),
-		this._root
-	);
-}
-
-function unRenderElement() {
-	render(h(Empty), this.shadowRoot, this._root);
-}
-
 function toVdom(element, nodeName) {
 	if (element.nodeType===3) return element.nodeValue;
 	if (element.nodeType!==1) return null;
-	let children=[], props={}, i=0, a=element.attributes, cn=element.childNodes;
-	for (i=a.length; i--; ) props[a[i].name] = a[i].value;
-	for (i=cn.length; i--; ) children[i] = toVdom(cn[i]);
+	let children=[], props={};
+	for (let att of element.attributes) { 
+		props[att.name] = att.value;
+	}
+	for (let child of element.childNodes) { 
+		children.push(this.toVdom(child));
+	}
 	return h(nodeName || element.nodeName.toLowerCase(), props, children);
+}
+
+
+function wrapPreactComponent(Component, tagName) {
+	return class extends HTMLElement {
+		constructor() {
+			super();
+			this.shadowRoot = this.attachShadow({mode: 'open'});
+			this._vdomComponent = Component;
+		}
+	
+		connectedCallback()  {
+			this.renderElement();
+		}
+	
+		attributeChangedCallback(attrName, oldVal, newVal)  {
+			this.renderElement();
+		}
+	
+		disconnectedCallback() {
+			this.unRenderElement();
+		}
+	
+	
+		renderElement() {
+			render(toVdom(this, this._vdomComponent), this.shadowRoot, this._root);			
+		}
+	
+		unrenderElement() {
+			render(h(Empty), this.shadowRoot, this._root);
+		}
+	}
+}
+
+export default function register(Component, tagName) {
+	return window.customElements.define(
+		tagName || Component.displayName || Component.name,
+		wrapPreactComponent(Component, tagName)
+	);
 }
