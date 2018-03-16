b744eb26effccd7093a9c6903a318d6dd17443d1
Bradley Spaulding <bspaulding@spredfast.com>
2018-03-16 10:17:18 -0700

    Added test case for #4

--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,5 @@
 import assert from 'assert';
-import { h, Component } from 'preact';
+import { h, Component, render } from 'preact';
 import registerElement from './index';
 
 class Clock extends Component {
@@ -28,6 +28,32 @@ it('renders ok, updates on attr change', function() {
 		root.innerHTML,
 		'<x-clock time="11:01:10 AM"><span>11:01:10 AM</span></x-clock>'
 	);
+
+	document.body.removeChild(root);
+});
+
+it('can rerender custom element with preact', function() {
+	const root = document.createElement('div');
+	document.body.appendChild(root);
+
+	let state = '10:08:07 AM';
+	class Clock extends Component {
+			render() {
+					return state;
+			}
+	}
+
+	registerElement(Clock, 'x-c');
+
+	function renderApp() {
+			render(<x-c/>, root, root.firstElementChild);
+	}
+
+	renderApp();
+	assert.equal(root.innerHTML, '<x-c>10:08:07 AM</x-c>');
+	state = '10:08:08 AM';
+	renderApp();
+	assert.equal(root.innerHTML, '<x-c>10:08:08 AM</x-c>');
 
 	document.body.removeChild(root);
 });
