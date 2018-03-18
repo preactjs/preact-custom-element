6187fdfacff3039c9e637b53ec5c375d45ea9b9c
Bradley Spaulding <bspaulding@spredfast.com>
2018-03-17 23:14:12 -0700

    Added notify reporter to karma conf

--- karma.conf.js
+++ karma.conf.js
@@ -38,7 +38,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'notify'],
 
 
     // web server port
