362336615630df5a1b62c8ef702142f5fb5731cb
Leah <me@hrmny.sh>
2019-10-17 21:35:45 +0200

    all the updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,35 @@
+const path = require('path');
+
 module.exports = {
-	entry: "./src/index.js",
+	entry: './src/index.js',
 	output: {
-		path: "dist",
-		filename: "bundle.js",
-		libraryTarget: "umd"
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+		libraryTarget: 'umd'
 	},
 	module: {
-		loaders: [{
-			test: /\.js$/,
-			exclude: /(node_modules|bower_components)/,
-			loader: "babel",
-			query: {
-				presets: ["es2015", "react"],
-				plugins: [
-					["transform-react-jsx", { "pragma":"h" }]
-				]
-			},
-		}]
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules|bower_components)/,
+				loader: 'babel-loader',
+				query: {
+					presets: [
+						['@babel/preset-env'],
+						[
+							'@babel/preset-react',
+							{
+								pragma: 'h',
+								pragmaFrag: 'Fragment'
+							}
+						]
+					]
+				}
+			}
+		]
 	},
+	mode: process.env.NODE_ENV === 'development' ? 'development' : 'production',
 	externals: {
-		preact: "preact"
+		preact: 'preact'
 	}
 };
