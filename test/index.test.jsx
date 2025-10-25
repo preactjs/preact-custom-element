cd588d43df4cb538d72306907fcebd5721b24de2
Ryan Christian <rchristian@ryanchristian.dev>
2025-10-25 15:38:13 -0500

    refactor: Drop kebab to camel name conversion

--- test/index.test.jsx
+++ test/index.test.jsx
@@ -210,39 +210,6 @@ describe('web components', () => {
 		);
 	});
 
-	const kebabName = 'custom-date-long-name';
-	const camelName = 'customDateLongName';
-	const lowerName = camelName.toLowerCase();
-	function PropNameTransform(props) {
-		return (
-			<span>
-				{props[kebabName]} {props[lowerName]} {props[camelName]}
-			</span>
-		);
-	}
-	registerElement(PropNameTransform, 'x-prop-name-transform', [
-		kebabName,
-		camelName,
-	]);
-
-	it('handles kebab-case attributes with passthrough', () => {
-		const el = document.createElement('x-prop-name-transform');
-		el.setAttribute(kebabName, '11/11/2011');
-		el.setAttribute(camelName, 'pretended to be camel');
-
-		root.appendChild(el);
-		assert.equal(
-			root.innerHTML,
-			`<x-prop-name-transform ${kebabName}="11/11/2011" ${lowerName}="pretended to be camel"><span>11/11/2011 pretended to be camel 11/11/2011</span></x-prop-name-transform>`
-		);
-
-		el.setAttribute(kebabName, '01/01/2001');
-		assert.equal(
-			root.innerHTML,
-			`<x-prop-name-transform ${kebabName}="01/01/2001" ${lowerName}="pretended to be camel"><span>01/01/2001 pretended to be camel 01/01/2001</span></x-prop-name-transform>`
-		);
-	});
-
 	const Theme = createContext('light');
 
 	function DisplayTheme() {
